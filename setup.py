a5f7724af90f55702f2011f0d79dec81f91b6638
ayushmankumar7 <ayushmankumar7@gmail.com>
2022-03-12 19:42:59 +0530

    setup complete

---
+++ setup.py
@@ -0,0 +1,10 @@
+"""
+I know this isn't standard but I feel it would be too much :)
+"""
+from src.main import install_pyopencl
+
+print("Installing PyOpenCL for Windows")
+print("Version 2022.1")
+print("\nRepo Author: Ayushman Kumar")
+
+install_pyopencl()
