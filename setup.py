1d57952c6558ea60105f5e152167ae64ca1f585f
ayushmankumar7 <ayushmankumar7@gmail.com>
2022-03-12 19:45:39 +0530

    setup success

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ from src.main import install_pyopencl
 print("Installing PyOpenCL for Windows")
 print("Version 2022.1")
 print("\nRepo Author: Ayushman Kumar")
+print("\nRepo URL: https://github.com/ayushmankumar7/PyOpenCL-Windows \n\n")
 
 install_pyopencl()
