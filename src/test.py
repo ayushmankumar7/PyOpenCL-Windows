b59455e1e1afeb1468d5b582ebd280d791196ab1
ayushmankumar7 <ayushmankumar7@gmail.com>
2022-03-12 19:28:06 +0530

    test completed

--- src/test.py
+++ src/test.py
@@ -1,4 +1,7 @@
 from utils import get_arch, get_version 
+from main import find_whl, install_pyopencl
 
 print(get_version(), get_arch())
 print(type(get_version()), type(get_arch()))
+print(find_whl())
+install_pyopencl()
